8a8f921ef94a1054c5642ee530786ed82aed4b81
Otavio Rodolfo Piske <angusyoung@gmail.com>
2022-06-09 10:36:49 +0200

    Updated to the latest Camel version and aligned deps

--- src/main/java/org/apache/camel/MyRouteBuilder.java
+++ src/main/java/org/apache/camel/MyRouteBuilder.java
@@ -3,30 +3,27 @@ package org.apache.camel;
 import ai.djl.modality.cv.output.DetectedObjects;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.dataformat.bindy.csv.BindyCsvDataFormat;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * A Camel Java DSL Router
  */
 public class MyRouteBuilder extends RouteBuilder {
+    private static final Logger LOG = LoggerFactory.getLogger(MyRouteBuilder.class);
 
     /**
      * Let's configure the Camel routing rules using Java code...
      */
     public void configure() {
-        BindyCsvDataFormat bindy = new BindyCsvDataFormat(ProbabilityRecord.class);
-
-        bindy.setLocale("default");
-
-        from("file:{{input.dir}}?recursive=true&noop=true")
+        from("file:src/main/resources/data?recursive=true&noop=true")
                 .convertBodyTo(byte[].class)
                 .to("djl:cv/object_detection?artifactId=ai.djl.mxnet:ssd:0.0.2")
-                .process(new Processor() {
-                    @Override
-                    public void process(Exchange exchange) throws Exception {
-                        DetectedObjects detectedObjects = exchange.getMessage().getBody(DetectedObjects.class);
+                .log("${header.CamelFileName} = ${body}")
+                .process(exchange -> {
+                    DetectedObjects detectedObjects = exchange.getMessage().getBody(DetectedObjects.class);
 
-                        detectedObjects.items().forEach(i -> System.out.println(i.getClassName()));
-                    }
+                    detectedObjects.items().forEach(i -> LOG.info("Class name: {}", i.getClassName()));
                 })
                 .to("mock:result");
     }
